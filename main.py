6759d63465e8322fa375904925026f5298572035
Rivares <ura-nikolaevic@yandex.ru>
2020-12-01 20:51:25 +0300

    Customizing manimlib

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+#!/usr/bin/env python
+
 from manimlib.imports import *
 
-class manim_examples(GraphScene):
+class AddingText(Scene):
+    #Adding text on the screen
     def construct(self):
+        my_first_text=TextMobject("Writing with manim is fun")
+        second_line=TextMobject("and easy to do!")
+        second_line.next_to(my_first_text,DOWN)
+        third_line=TextMobject("for me and you!")
+        third_line.next_to(my_first_text,DOWN)
 
-        self.setup_axes()
+        self.add(my_first_text, second_line)
+        self.wait(2)
+        self.play(Transform(second_line,third_line))
+        self.wait(2)
+        second_line.shift(3*DOWN)
+        self.play(ApplyMethod(my_first_text.shift,3*UP))
 
-        graph_1 = self.get_graph(lambda x: (1/2) * x ** 2 - 3)
+class PrintText1(Scene):
+    def construct(self):
+        txt = TexMobject("\\text dvsdrh\\quad\\ \\color{red}gaeadwa\\color{black}\\quad \\sum_{k=1}^\\infty {1 \\over k^2} = {\\pi^2 \\over 6}")
 
-        self.play(ShowCreation(graph_1))
+        self.play(Write(txt), run_time=5)
         self.wait()
-
 
 if __name__ == '__main__':
     title = TextMobject("This is some LaTeX")
